5e01afb1d1f204d6bf74d7479cb5e498071fc540
albertofanini-prog <albertofanini@outlook.com>
2022-03-31 14:08:46 +0200

    Upload html & css from repo vue-dischi

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
 <?php
     var_dump('Php ajax dischi');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP AJAX Dischi</title>
+</head>
+<body>
+    <header>
+        <div class="header_container">
+            <img src="spotyLogo.png">
+            </select> 
+        </div>
+    </header>
+    <main>
+        <section class="main_section">
+            <div class="album_container">
+                <div class="album_wrapper"> 
+                    <div class="album">
+                        <img src="">
+                        <h5></h5>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
